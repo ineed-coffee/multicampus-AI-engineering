40315812e133f27df9dba96a36eef950132cef3b
ineedcoffee <leey93ssu@gmail.com>
2020-10-16 14:06:22 +0900

    small

---
+++ R을 활용한 데이터 분석/20.10.16/day9.r
@@ -0,0 +1,38 @@
+library(readxl)
+
+groceries <- read.csv('C:/임시/RData/groceries.csv')
+
+str(groceries)
+
+install.packages('arules')
+library(arules)
+groceries <- read.transactions('C:/임시/RData/groceries.csv',sep=",")
+summary(groceries)
+
+itemFrequency(groceries[,1:10])
+# itemFrequency 함수로 거래 비율을 확인 : Suppory(지지도)
+
+itemFrequencyPlot(groceries,support=0.08)
+itemFrequencyPlot(groceries,topN=20)
+
+image(groceries[1:5])
+
+groceryRules <- apriori(groceries,
+                        parameter=list(support=0.005,
+                                       confidence=0.25,
+                                       minlen=2))
+# minlen : x->y 의 연관규칙에서 아이템이 minlen 미만인 것들은
+# 고려하지 않음음
+summary(groceryRules)
+
+inspect(groceryRules[1:5])
+
+inspect(sort(groceryRules, by='lift',decreasing=FALSE)[1:5])
+
+
+# 전략적으로 berries 상품과 함께 향상도가 높은 상품들을 추출하고자한다.
+berryRules <- subset(groceryRules,items %in% 'berries')
+
+write(berryRules,file='berryRules.csv',sep=',',row.names=F)
+
+as
---
+++ R을 활용한 데이터 분석/20.10.16/note.md
@@ -0,0 +1,9 @@
+__new installed packages__ 
+
+- arules : package for association rules
+
+
+
+다음 주 , 
+
+- r로 회귀모델 , 신경망
